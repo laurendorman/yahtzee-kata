8f67570dbfcded4c6cc6ec04086fb3d3e4a724e6
Lauren Dorman <lauren@heysparkbox.com>
2015-02-17 11:51:17 -0500

    5 scoring tests passing

--- lib/score.rb
+++ lib/score.rb
@@ -1,12 +1,25 @@
 class Score
 
-  def standard_scoring(score)
-    score.inject(:+)
+  def standard_scoring(dice)
+    dice.inject(:+)
   end
 
-  def full_house_scoring(score)
-    score.uniq.collect { |die| score.count(die) }.sort == [2, 3]
-    return 25
+  def full_house_scoring(dice)
+    (dice.uniq.collect { |die| dice.count(die) }.sort == [2, 3]) ? 25 : 0
+  end
+
+  def small_straight_scoring(dice)
+    (dice.sort[0..3] == [1,2,3,4] || dice.sort[1..4] == [1,2,3,4] || dice.sort[0..3] == [2,3,4,5] || dice.sort[1..4] == [2,3,4,5] || dice.sort[0..3] == [3,4,5,6] || dice.sort[1..4] == [3,4,5,6]) ? 30 : 0
+  end
+
+  def large_straight_scoring(dice)
+    dice.sort == [1,2,3,4,5] || dice.sort == [2,3,4,5,6]
+    dice.inject(:+)
+  end
+
+  def yahtzee_scoring(dice)
+    dice.uniq.count == 1
+    dice.inject(:+)
   end
 
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -9,6 +9,18 @@ describe Score do
     expect(subject.standard_scoring([3,3,3,3,5])).to eq(17)
   end
   it 'full house scoring' do
-    expect(subject.full_house_scoring([1,2,3,4,5])).to eq(25)
+    expect(subject.full_house_scoring([2,2,3,3,3])).to eq(25)
+  end
+  it 'small straight scoring' do
+    expect(subject.small_straight_scoring([1,2,3,4,6])).to eq(30)
+  end
+  it 'large straight scoring' do
+    expect(subject.large_straight_scoring([2,3,4,5,6])).to eq(40)
+  end
+  it 'large straight scoring' do
+    expect(subject.yahtzee_scoring([2,2,2,2,2])).to eq(50)
+  end
+  it 'chance scoring' do
+    expect(subject.standard_scoring([2,2,4,5,6])).to eq(19)
   end
 end
